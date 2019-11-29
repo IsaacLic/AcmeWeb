a3278836b06c12f96e504d1ba3435d7effc55bc7
Isaac Lichter <isaacilichter@gmail.com>
2019-11-29 15:21:19 -0500

    Cleaned up StatusController

--- src/main/java/com/acme/statusmgr/StatusController.java
+++ src/main/java/com/acme/statusmgr/StatusController.java
@@ -65,23 +65,4 @@ public class StatusController {
         return serverStatus;
     }
 
-    /**
-     * selects which server status detail to be added, then adds it
-     *
-     * @param serverStatus the server status to be added to
-     * @param detail       which detail to add
-     * @return the augmented server status
-     */
-    private ServerStatus decorateServerStatus(ServerStatus serverStatus, String detail) {
-        switch (detail) {
-            case "extensions":
-                return new ExtensionsDecorator(serverStatus);
-            case "memory":
-                return new MemoryDecorator(serverStatus);
-            case "operations":
-                return new OperationsDecorator(serverStatus);
-            default:
-                throw new BadRequestException("Invalid details option: " + detail);
-        }
-    }
 }
