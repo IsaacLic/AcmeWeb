76c55e0d370dc7f81cb4c8d0981358467d2640ca
Isaac Lichter <isaacilichter@gmail.com>
2019-11-28 23:58:15 -0500

    Maintains the reference to the serverManager

--- src/main/java/com/acme/beans/ServerStatusDecorator.java
+++ src/main/java/com/acme/beans/ServerStatusDecorator.java
@@ -12,6 +12,7 @@ public abstract class ServerStatusDecorator extends ServerStatus {
     private final ServerStatus serverStatus;
 
     public ServerStatusDecorator(ServerStatus serverStatus) {
+        super(serverStatus.getId(),serverStatus.getContentHeader());
         this.serverStatus = serverStatus;
     }
 
