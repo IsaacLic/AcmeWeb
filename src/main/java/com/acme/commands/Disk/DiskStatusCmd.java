c578ca6d06dc0771fe7138ab92177b43847a4224
Isaac Lichter <isaacilichter@gmail.com>
2020-01-02 21:22:36 -0500

    Created a singleton proxy for GenerateDiskStatusCmd

--- src/main/java/com/acme/commands/Disk/DiskStatusCmd.java
+++ src/main/java/com/acme/commands/Disk/DiskStatusCmd.java
@@ -15,19 +15,25 @@ public class DiskStatusCmd implements StatusCommand {
     private final long Id;
     private final String template;
     private final String name;
+    private final GenerateDiskStatusCmd cmd;
 
     private DiskStatus status;
 
-    public DiskStatusCmd(long Id, String template, String name) {
+    public DiskStatusCmd(long Id, String template, String name, GenerateDiskStatusCmd cmd) {
         this.Id = Id;
         this.template = template;
         this.name = name;
+        this.cmd = cmd;
+
         executor = new SimpleCommandExecutor();
+    }
+
+    public DiskStatusCmd(long Id, String template, String name) {
+        this(Id, template, name, new GenerateDiskStatusCmd());
     }
 
     @Override
     public void execute() {
-        GenerateDiskStatusCmd cmd = new GenerateDiskStatusCmd();
         executor.executeCommand(cmd);
         status = new DiskStatus(Id, String.format(template, name));
         status.setStatusDesc(cmd.getResult());
